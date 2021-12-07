ee1048c36a152682d055d99ee79bf5f50bfd0887
bok1949 <bryan.vigilia.flores@gmail.com>
2021-12-08 02:33:32 +0800

    update on index file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,7 @@
                 <hr>
                 <form action="{{ route('auth.check') }}" method="POST">
                     @csrf
+                    {{ method_field('PUT') }}
                     <div class="results">
                         @if (Session::get('fail'))
                             <div class="alert alert-danger">
