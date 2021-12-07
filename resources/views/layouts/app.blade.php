5183cf589541bf50495a178d1932ba0f3d398c9f
bok1949 <bryan.vigilia.flores@gmail.com>
2021-12-07 22:44:37 +0800

    app view update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,11 @@
 
         <!-- CoreUI CSS -->
         {{-- Scripts --}}
-        <script src="{{ url('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
         {{-- <script src="{{ asset('js/bootstrap.js') }}" defer></script> --}}
 
         {{-- Styles --}}
-        <link rel="stylesheet" href="{{url('css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         
         @livewireStyles
 
