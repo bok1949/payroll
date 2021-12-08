3971ac9eefd2e1bbc0ce0353065994f8e55cb8e4
bok1949 <bryan.vigilia.flores@gmail.com>
2021-12-08 17:23:32 +0800

    updates

--- config/logging.php
+++ config/logging.php
@@ -5,7 +5,17 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
 return [
-
+    'default' => env('LOG_CHANNEL', 'stack'),
+        'channels' => [
+            'stack' => [
+                'driver' => 'stack',
+                'channels' => ['single'],
+            ],
+            'single' => [
+                'driver' => 'errorlog',
+                'level' => 'debug',
+            ],
+        ],
     /*
     |--------------------------------------------------------------------------
     | Default Log Channel
